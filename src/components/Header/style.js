cfd2b8783322904a781b0fa3a41855452ddef0bd
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-03-16 21:40:55 +0100

    :lipstick: simple header styles

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,10 +1,22 @@
 import styled from "styled-components";
+import { colors } from "../../styles/variables";
 
 export const HeaderEl = styled.header`
-  nav {
-    ul {
-      li {
-      }
+  padding: 1rem 0;
+  h1 {
+    margin: 0;
+    position: relative;
+
+    &::after {
+      content: "";
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 0.4rem;
+      background-color: ${colors.orange};
+      z-index: -1;
     }
   }
 `;
