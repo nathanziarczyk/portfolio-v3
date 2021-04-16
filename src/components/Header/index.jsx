cd1f72e6d6014e9abf495ec3e2d438b5b3ab8d94
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-16 23:39:05 +0200

    :sparkles: added navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,29 @@
+import { Link } from "gatsby";
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import { HeaderEl } from "./style";
 
 const Header = () => {
   return (
-    <HeaderEl>
-      <Container>
-        <Row>
-          <h1>Nathan</h1>
-        </Row>
-      </Container>
-    </HeaderEl>
+    <Container>
+      <Row>
+        <HeaderEl>
+          <h1>
+            <Link to="/">Nathan</Link>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </HeaderEl>
+      </Row>
+    </Container>
   );
 };
 
