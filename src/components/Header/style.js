2e7d8f6928150bb92f18c72f95d989525c7c8b3c
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-16 23:39:18 +0200

    :lipstick: added navigation styles

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,21 +2,47 @@ import styled from "styled-components";
 import { colors } from "../../styles/variables";
 
 export const HeaderEl = styled.header`
-  padding: 1rem 0;
+  padding: 2rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
   h1 {
     margin: 0;
-    position: relative;
 
-    &::after {
-      content: "";
-      position: absolute;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 0.4rem;
-      background-color: ${colors.orange};
-      z-index: -1;
+    a {
+      position: relative;
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        right: -5px;
+        left: -5px;
+        height: 40%;
+        background-color: ${colors.accent};
+        z-index: -1;
+        opacity: 0.45;
+      }
+    }
+  }
+
+  nav {
+    ul {
+      display: flex;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+
+      li {
+        &:not(:last-child) {
+          margin-right: 2.5rem;
+        }
+
+        a {
+          font-size: 1.25rem;
+          font-weight: 600;
+        }
+      }
     }
   }
 `;
