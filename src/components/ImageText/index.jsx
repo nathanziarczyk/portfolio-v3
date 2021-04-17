a1a3e19eb51143e1e88a89d9bdf6525dac7c0794
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-17 08:48:28 +0200

    :sparkles: added dummy content to image-text

---
+++ src/components/ImageText/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Wrapper } from "../../styles/base";
+import { Content, ImageTextWrapper, Image } from "./style";
+import headshot from "../../images/me_web_2.png";
+
+const ImageText = () => {
+  return (
+    <ImageTextWrapper>
+      <Wrapper style={{ paddingBottom: 0 }}>
+        <Container>
+          <Row>
+            <Col lg={6}>
+              <Content>
+                <h1>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </Content>
+            </Col>
+            <Col lg={6}>
+              <Image>
+                <img src={headshot} alt="" />
+              </Image>
+            </Col>
+          </Row>
+        </Container>
+      </Wrapper>
+    </ImageTextWrapper>
+  );
+};
+
+export default ImageText;
