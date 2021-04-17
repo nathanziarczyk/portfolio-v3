b0e1ae033c32c0577f05f67906176680d8f7aafb
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-17 09:01:44 +0200

    :lipstick: added overlay over image to blend in better

--- src/components/ImageText/style.js
+++ src/components/ImageText/style.js
@@ -13,8 +13,20 @@ export const Content = styled.div`
   padding-bottom: 6rem;
 `;
 export const Image = styled.div`
+  position: relative;
   height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: ${colors.primary};
+    opacity: 0.15;
+  }
 `;
