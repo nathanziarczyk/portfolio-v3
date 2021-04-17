1cebe0ff9a5d3854ef0b064dbcc16de660f955d0
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-17 08:29:57 +0200

    :sparkles: added dummy content to hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,10 +1,34 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Wrapper } from "../../styles/base";
 import { HeroWrapper } from "./style";
 
 const Hero = () => {
-  return null;
+  return (
+    <Wrapper>
+      <HeroWrapper>
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col lg={8}>
+              <h1>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </HeroWrapper>
+    </Wrapper>
+  );
 };
 
 export default Hero;
