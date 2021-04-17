231b82f35ebf806873a4f89b7c49bf159dff4cc4
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-17 09:14:17 +0200

    :mag:  use seo component on all pages

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../Header";
+import SEO from "../SEO";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <SEO />
       <Header />
       {children}
     </>
