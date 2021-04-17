bdfa0e41fabe007709b4c6b9b381b53f9936af4b
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-17 08:28:50 +0200

    :recycle:

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,29 @@
 import { Link } from "gatsby";
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { HeaderEl } from "./style";
 
 const Header = () => {
   return (
     <Container>
       <Row>
-        <HeaderEl>
-          <h1>
-            <Link to="/">Nathan</Link>
-          </h1>
-          <nav>
-            <ul>
-              <li>
-                <a href="/">About</a>
-              </li>
-              <li>
-                <a href="/">Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </HeaderEl>
+        <Col>
+          <HeaderEl>
+            <h2>
+              <Link to="/">Nathan</Link>
+            </h2>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">About</a>
+                </li>
+                <li>
+                  <a href="/">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </HeaderEl>
+        </Col>
       </Row>
     </Container>
   );
