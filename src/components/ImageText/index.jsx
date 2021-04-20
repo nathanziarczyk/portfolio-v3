0cff8ab3e04032af3d61fd72d446637702896efe
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-04-20 08:36:22 +0200

    :recycle: content

--- src/components/ImageText/index.jsx
+++ src/components/ImageText/index.jsx
@@ -3,28 +3,36 @@ import { Col, Container, Row } from "react-bootstrap";
 import { Wrapper } from "../../styles/base";
 import { Content, ImageTextWrapper, Image } from "./style";
 import headshot from "../../images/me_web_2.png";
+import { Button } from "../Button/style";
 
 const ImageText = () => {
   return (
     <ImageTextWrapper>
-      <Wrapper style={{ paddingBottom: 0 }}>
+      <Wrapper style={{ paddingBottom: 0, paddingTop: "4rem" }}>
         <Container>
           <Row>
-            <Col lg={6}>
+            <Col lg={7}>
               <Content>
-                <h2>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor
-                </h2>
+                <h2>Let me help you give your company the boost it needs</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Now more than ever, a good digital appearance is key to the
+                  success of your company. A user friendly, professional looking
+                  and unique website or webshop is a great investment to attract
+                  new clients.
                 </p>
+                <p>
+                  My experience with platforms like Wordpress, Shopify, Drupal,
+                  and others allows me to select the right tools for the job.
+                  This way you can be sure you'll get the most out of your
+                  investment.
+                </p>
+                <p>Sounds good?</p>
+                <div className="links">
+                  <Button primary>Get in touch</Button>
+                </div>
               </Content>
             </Col>
-            <Col lg={6}>
+            <Col lg={5}>
               <Image>
                 <img src={headshot} alt="" />
               </Image>
