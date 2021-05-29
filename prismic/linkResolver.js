34c3c9287ff06ce32700612f79d670431b73a353
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-05-29 12:09:37 +0200

    :sparkles: prismic linkResolver

---
+++ prismic/linkResolver.js
@@ -0,0 +1,8 @@
+const linkResolver = function (doc) {
+  // Pretty URLs for known types
+  if (doc.type === "blog") return "/post/" + doc.uid;
+  if (doc.type === "page") return "/" + doc.uid;
+  // Fallback for other types, in case new custom types get created
+  return "/doc/" + doc.id;
+};
+module.exports = linkResolver;
