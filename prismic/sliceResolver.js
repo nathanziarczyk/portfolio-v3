fa04de0d41607690bfeac8db49235385f586264e
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-05-29 12:45:13 +0200

    :sparkles: resolve image text slice

--- prismic/sliceResolver.js
+++ prismic/sliceResolver.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "../src/components/Hero";
+import ImageText from "../src/components/ImageText";
 
 export const sliceResolver = (slice, key) => {
   switch (slice.slice_type) {
     case "hero":
       return <Hero data={slice} key={key} />;
 
+    case "image_text":
+      return <ImageText data={slice} key={key} />;
     default:
       break;
   }
