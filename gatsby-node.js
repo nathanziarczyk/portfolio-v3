4314364b3614371e6be4e1d7c5147ac210af0a63
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-05-29 12:09:10 +0200

    :sparkles: basic createPages function

---
+++ gatsby-node.js
@@ -0,0 +1,28 @@
+const path = require("path");
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const pages = await graphql(`
+    {
+      allPrismicPage {
+        nodes {
+          id
+          uid
+          lang
+          type
+          url
+        }
+      }
+    }
+  `);
+
+  pages.data.allPrismicPage.nodes.forEach((page) => {
+    createPage({
+      path: page.uid,
+      component: path.resolve(__dirname, "src/templates/Page/index.js"),
+      context: { ...page },
+    });
+  });
+};
