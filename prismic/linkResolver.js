011e1ca67747f129ca16cc13cc2c18d917803098
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-06 22:49:53 +0200

    :bug:

--- prismic/linkResolver.js
+++ prismic/linkResolver.js
@@ -1,4 +1,4 @@
-module.exports = linkResolver = function (link) {
+module.exports = function linkResolver(link) {
   console.log(link);
   if (link.link_type === "Document") return "/" + link.uid;
   // Fallback for other types, in case new custom types get created
