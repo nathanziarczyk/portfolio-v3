66257f3271e9f345ffa61b95c5430380c4a14fcb
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-08 21:41:04 +0200

    :lipstick: apply bg when needed

--- src/styles/base.js
+++ src/styles/base.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
-import { theme } from "./theme";
 
 export const Wrapper = styled.div`
   padding: 6rem 0;
-  background-color: ${({ bg }) => (bg === "with_bg" ? theme.primary : "none")};
+  ${({ bg, theme }) => {
+    if (bg === "with_bg") {
+      return `background-color: ${theme.primary}`;
+    }
+  }};
 `;
