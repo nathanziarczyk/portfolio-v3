c6819c7b0aafc97a55402359a2bd2fb1097b534e
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-08 22:50:38 +0200

    :package: font awesome

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -27,6 +27,13 @@ const SEO = ({ title, description, image, article }) => {
         href="https://fonts.googleapis.com/css2?family=Orelega+One&family=Raleway:wght@400;600;700&display=swap"
         rel="stylesheet"
       ></link>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       {seo.url && <meta property="og:url" content={seo.url} />}
