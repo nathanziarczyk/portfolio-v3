581b12c409fab58032777c306f69c4ab8789f183
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-08 21:30:47 +0200

    :lipstick: add background based on theme

--- src/styles/base.js
+++ src/styles/base.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import { theme } from "./theme";
 
 export const Wrapper = styled.div`
   padding: 6rem 0;
+  background-color: ${({ bg }) => (bg === "with_bg" ? theme.primary : "none")};
 `;
