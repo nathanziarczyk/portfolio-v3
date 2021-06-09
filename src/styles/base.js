4a6cb001fc02da0491e7842155be2992d7d48297
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-09 22:55:13 +0200

    :lipstick: position absolute particles

--- src/styles/base.js
+++ src/styles/base.js
@@ -1,10 +1,21 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  position: relative;
+  overflow: visible;
   padding: 6rem 0;
   ${({ bg, theme }) => {
     if (bg === "with_bg") {
       return `background-color: ${theme.primary}`;
     }
   }};
+
+  #tsparticles {
+    position: absolute;
+    top: -10rem;
+    z-index: -1;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
 `;
