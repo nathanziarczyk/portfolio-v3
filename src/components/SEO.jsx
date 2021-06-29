e187278e28f7c36bb7a3a41699a4e94e0bbebafb
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-29 11:00:49 +0200

    :poop:

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -27,12 +27,16 @@ const SEO = ({ title, description, image, article }) => {
         href="https://fonts.googleapis.com/css2?family=Orelega+One&family=Raleway:wght@400;600;700&display=swap"
         rel="preload"
         as="font"
+        type="font/woff2"
+        crossOrigin
       ></link>
       <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
         rel="preload"
         as="font"
+        type="font/woff2"
+        crossOrigin
       />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
