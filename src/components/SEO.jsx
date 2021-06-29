191a85ff104915bee25d5c373eb6de9111e71010
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-29 10:57:33 +0200

    :bug: preload as font

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -26,13 +26,13 @@ const SEO = ({ title, description, image, article }) => {
       <link
         href="https://fonts.googleapis.com/css2?family=Orelega+One&family=Raleway:wght@400;600;700&display=swap"
         rel="preload"
-        as="style"
+        as="font"
       ></link>
       <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
         rel="preload"
-        as="style"
+        as="font"
       />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
