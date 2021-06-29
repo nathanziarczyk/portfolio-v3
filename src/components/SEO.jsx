d38c15c4b39618b972b32fbe80ef0323c5e8f75a
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-06-29 11:05:54 +0200

    :poop: abandon it for now, i'll come back to it

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -23,20 +23,10 @@ const SEO = ({ title, description, image, article }) => {
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Orelega+One&family=Raleway:wght@400;600;700&display=swap"
-        rel="preload"
-        as="font"
-        type="font/woff2"
-        crossOrigin
-      ></link>
+      <link href="https://fonts.googleapis.com/css2?family=Orelega+One&family=Raleway:wght@400;600;700&display=swap"></link>
       <link
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-        rel="preload"
-        as="font"
-        type="font/woff2"
-        crossOrigin
       />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
