1bf5e9baf0557fe94605bba220758f97ba27eb6c
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-07-03 22:16:24 +0200

    :iphone: smaller margins/paddings on mobile wrapper

--- src/styles/base.js
+++ src/styles/base.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { variables } from "./variables";
 
 export const Wrapper = styled.div`
   position: relative;
@@ -17,4 +18,20 @@ export const Wrapper = styled.div`
       `;
     }
   }};
+
+  @media screen and (max-width: ${variables.md}) {
+    ${({ bg, theme }) => {
+      if (bg === "with_bg") {
+        return `
+        padding: 3rem 0;
+        background-color: ${theme.primary};
+      `;
+      } else {
+        return `
+        padding: 0;
+        margin: 3rem 0;
+      `;
+      }
+    }};
+  }
 `;
