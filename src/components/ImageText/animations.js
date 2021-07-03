3578f7339cd7ffad06436c6d21efd952cce7e537
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-07-03 16:49:20 +0200

    :dizzy: animate story content in when in view

---
+++ src/components/ImageText/animations.js
@@ -0,0 +1,25 @@
+export const variants = {
+  image: {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.65,
+        delay: 0.15,
+      },
+    },
+  },
+  content: {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.95,
+      },
+    },
+  },
+};
--- src/components/ImageText/index.jsx
+++ src/components/ImageText/index.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Wrapper } from "../../styles/base";
 import { Content, ImageTextWrapper, Image } from "./style";
 import { Button } from "../Button/style";
+import { motion, useAnimation } from "framer-motion";
+import { variants } from "./animations";
+import { useInView } from "react-intersection-observer";
 
 const ImageText = ({ data }) => {
   const { content, direction, image, theme } = data.primary;
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
-    <ImageTextWrapper>
+    <ImageTextWrapper ref={ref}>
       <Wrapper bg={theme} style={{ paddingBottom: 0, paddingTop: "4rem" }}>
         <Container>
           <Row>
             <Col lg={7}>
-              <Content
-                dangerouslySetInnerHTML={{ __html: content.html }}
-              ></Content>
+              <motion.div
+                initial="hidden"
+                animate={controls}
+                variants={variants.content}
+              >
+                <Content
+                  dangerouslySetInnerHTML={{ __html: content.html }}
+                ></Content>
+              </motion.div>
             </Col>
             <Col lg={5}>
               <Image>
-                <img src={image.fluid.src} alt={image.alt} />
+                <motion.img
+                  src={image.fluid.src}
+                  alt={image.alt}
+                  initial="hidden"
+                  animate={controls}
+                  variants={variants.image}
+                />
               </Image>
             </Col>
           </Row>
