f4c36a91ac2148fb72cff6c0ffaf034b759b97eb
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-07-04 20:42:37 +0200

    :bug: ?

--- src/components/PageHeader/style.js
+++ src/components/PageHeader/style.js
@@ -1,14 +1,8 @@
 import styled from "styled-components";
 import { Wrapper } from "../../styles/base";
 
-export const PageHeaderWrapper = styled(Wrapper)`
-  ${({ bg }) => {
-    if (bg === "with_bg") {
-      return `padding: 1rem 0;`;
-    } else {
-      return `margin: 1rem 0;`;
-    }
-  }}
+export const PageHeaderWrapper = styled.div`
+  margin: 1rem 0;
 `;
 
 export const Title = styled.h1`
