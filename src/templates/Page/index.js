8f8fc99825439568eb0bd7cef1685345cd3e5b2c
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-07-11 22:29:26 +0200

    :sparkles: image alignment variable on imagetext

--- src/templates/Page/index.js
+++ src/templates/Page/index.js
@@ -68,6 +68,7 @@ export const query = graphql`
                 primary {
                   theme
                   direction
+                  image_alignment
                   content {
                     html
                   }
