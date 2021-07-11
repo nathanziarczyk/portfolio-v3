fcc16796784adbfc50a03b8d40d241bfa965b8ce
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-07-11 14:48:48 +0200

    :sparkles: footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,53 @@
+import { Link } from "gatsby";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { FooterWrapper } from "./style";
+import { FooterTab, FooterWrapper } from "./style";
 
 const Footer = () => {
   return (
     <FooterWrapper>
       <Container>
         <Row>
-          <Col>
-            <p>© {new Date().getFullYear()} - Nathan Ziarczyk</p>
+          <Col lg={3}>
+            <h2>
+              <Link className="disable-global" href="/">
+                Nathan
+              </Link>
+            </h2>
+          </Col>
+          <Col lg={3}>
+            <FooterTab>
+              <h3>Sitemap</h3>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact</Link>
+                  </li>
+                </ul>
+              </nav>
+            </FooterTab>
+          </Col>
+          <Col lg={3}>
+            <FooterTab>
+              <h3>Contact</h3>
+              <ul>
+                <li>
+                  <b>Nathan Ziarcyk</b>
+                </li>
+                <li>
+                  <a href="mailto:hello@nathanz.dev">hello@nathanz.dev</a>
+                </li>
+                <li>BTW: BE0733.981.776</li>
+              </ul>
+            </FooterTab>
+          </Col>
+          <Col lg={3}>
+            <FooterTab>
+              <p>© {new Date().getFullYear()} - Nathan Ziarczyk</p>
+            </FooterTab>
           </Col>
         </Row>
       </Container>
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,9 +1,50 @@
 import styled from "styled-components";
+import { variables } from "../../styles/variables";
 
 export const FooterWrapper = styled.div`
-  padding: 0.5rem 0;
+  padding: 2rem 0;
 
   p {
     margin: 0;
   }
+
+  h2 {
+    margin: 0;
+    font-size: 2.5rem;
+
+    a {
+      position: relative;
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        right: -5px;
+        left: -5px;
+        height: 40%;
+        background-color: ${({ theme }) => theme.accent};
+        z-index: -1;
+        opacity: 0.45;
+      }
+    }
+  }
+`;
+
+export const FooterTab = styled.div`
+  h3 {
+    margin-bottom: 1rem;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    li,
+    a {
+      color: ${({ theme }) => theme.text};
+    }
+  }
+  @media screen and (max-width: ${variables.lg}) {
+    margin-top: 1.5rem;
+  } ;
 `;
