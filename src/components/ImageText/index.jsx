05341b15e1f8df3839c9b87a3bd78abf88073988
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-08-05 21:22:13 +0200

    :recycle: re-enable animations on image text

--- src/components/ImageText/index.jsx
+++ src/components/ImageText/index.jsx
@@ -12,38 +12,40 @@ const ImageText = ({ data }) => {
   let { content, direction, image, theme, image_alignment } = data.primary;
   const [ref, controls] = useScrollAnimation();
   return (
-    <ImageTextWrapper bg={theme} imageAlignment={image_alignment} ref={ref}>
-      <Row>
-        <Col sm={6}>
-          <motion.div
-            // initial="hidden"
-            // animate={controls}
-            // variants={variants.content}
-            style={{ height: "100%" }}
-          >
-            <Content
-              imageAlignment={image_alignment}
-              dangerouslySetInnerHTML={{ __html: content.html }}
-            ></Content>
-          </motion.div>
-        </Col>
-        <Col sm={5} className="offset-sm-1">
-          <Parallax y={[-2.5, 2.5]}>
-            <Image>
-              <motion.img
-                src={image.fluid.src}
-                alt={image.alt}
-                // initial="hidden"
-                // animate={controls}
-                // variants={variants.image}
-                width={600}
-                height={500}
-              />
-            </Image>
-          </Parallax>
-        </Col>
-      </Row>
-    </ImageTextWrapper>
+    <div ref={ref}>
+      <ImageTextWrapper bg={theme} imageAlignment={image_alignment}>
+        <Row>
+          <Col sm={6}>
+            <motion.div
+              initial="hidden"
+              animate={controls}
+              variants={variants.content}
+              style={{ height: "100%" }}
+            >
+              <Content
+                imageAlignment={image_alignment}
+                dangerouslySetInnerHTML={{ __html: content.html }}
+              ></Content>
+            </motion.div>
+          </Col>
+          <Col sm={5} className="offset-sm-1">
+            <Parallax y={[-2.5, 2.5]}>
+              <Image>
+                <motion.img
+                  src={image.fluid.src}
+                  alt={image.alt}
+                  initial="hidden"
+                  animate={controls}
+                  variants={variants.image}
+                  width={600}
+                  height={500}
+                />
+              </Image>
+            </Parallax>
+          </Col>
+        </Row>
+      </ImageTextWrapper>
+    </div>
   );
 };
 
