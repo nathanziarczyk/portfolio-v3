a666af53791bde973337d5d32b3912c1c3b747f6
Nathan Ziarczyk <nathanziarczyk1997@outlook.com>
2021-08-30 00:06:33 +0200

    :lipstick: button hover styles

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -11,15 +11,36 @@ export const Button = styled.button`
   letter-spacing: 1px;
   border-radius: 1.5rem;
   cursor: pointer;
-  color: ${({ theme, _type }) =>
+  color: ${({ theme, _type = "primary" }) =>
     _type === "secondary" ? theme.primary : theme.button_color};
   border: 2px solid ${({ theme }) => theme.accent};
   background-color: ${({ theme }) => theme.accent};
-  background-color: ${({ _type }) => _type === "secondary" && "unset"};
+  background-color: ${({ _type = "primary" }) =>
+    _type === "secondary" && "unset"};
   position: relative;
-
+  transition: color 0.3s ease-in-out, background-color 0.3s ease;
   a {
+    transition: color 0.3s ease-in-out;
     color: ${({ theme, _type }) =>
       _type === "secondary" ? theme.primary : theme.button_color};
+  }
+
+  &:hover {
+    ${({ _type = "primary", theme }) => {
+      return _type === "primary"
+        ? `
+        background-color: unset;
+        color: ${theme.primary};
+        a{
+          color: ${theme.primary};
+        }
+      `
+        : `
+        background-color: ${theme.primary};
+        color: ${theme.bg};
+        a{
+          color: ${theme.bg};
+        }`;
+    }}
   }
 `;
