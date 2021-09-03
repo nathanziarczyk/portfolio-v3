f029c9fc150ecba8465368f25e9a8844deefd787
Nathan Ziarczyk <nathan@xlab.be>
2021-09-03 12:57:25 +0200

    :sparkles: add carousel to logobar

--- src/components/slices/Logobar/Logobar.jsx
+++ src/components/slices/Logobar/Logobar.jsx
@@ -1,11 +1,14 @@
 import { motion } from "framer-motion";
 import { Link } from "gatsby";
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Carousel, Col, Container, Row } from "react-bootstrap";
 import linkResolver from "../../../../prismic/linkResolver";
 import { useScrollAnimation } from "../../../hooks/useScrollAnimation";
 import { variants } from "./animations";
-import { LogobarWrapper } from "./style";
+import { LogobarSlider, LogobarWrapper } from "./style";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Logobar = ({ data }) => {
   const {
@@ -13,6 +16,45 @@ const Logobar = ({ data }) => {
     items,
   } = data;
   const [ref, controls] = useScrollAnimation();
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 0,
+    arrows: false,
+    cssEase: "linear",
+    swipe: false,
+    speed: 5000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
   return (
     <div ref={ref}>
       <LogobarWrapper type={theme}>
@@ -28,7 +70,7 @@ const Logobar = ({ data }) => {
                   {title.text}
                 </motion.h2>
               )}
-              <div className="logos">
+              <LogobarSlider {...settings} className="logos">
                 {items.map((logo, i) => {
                   return (
                     <Link to={linkResolver(logo.link)} target="_blank">
@@ -50,7 +92,7 @@ const Logobar = ({ data }) => {
                     </Link>
                   );
                 })}
-              </div>
+              </LogobarSlider>
             </Col>
           </Row>
         </Container>
--- src/components/slices/Logobar/style.js
+++ src/components/slices/Logobar/style.js
@@ -1,28 +1,34 @@
 import styled from "styled-components";
 import { variables } from "../../../styles/variables";
 import Wrapper from "../../Wrapper";
+import Slider from "react-slick";
 
 export const LogobarWrapper = styled(Wrapper)`
   text-align: center;
   .logos {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    &:empty {
-      display: none;
-    }
+    width: 70%;
+    margin: 3rem auto 0;
     a {
       opacity: 0.85;
       transition: opacity 0.5s ease-in-out;
-      margin: 1.5rem 1.05rem 0;
       img {
-        height: 3.25rem;
+        display: block;
+        margin: 0 auto;
         width: auto;
+        height: auto;
+        max-width: 8rem;
+        max-height: 5rem;
       }
 
       &:hover {
         opacity: 0.5;
       }
     }
+
+    @media screen and (max-width: ${variables.lg}) {
+      width: 100%;
+    }
   }
 `;
+
+export const LogobarSlider = styled(Slider)``;
