1ccfd46f1704e5feb9eae1777cc01092281d9293
Nathan Ziarczyk <nathan.ziar@gmail.com>
2022-01-04 17:16:39 +0100

    gradient component

---
+++ src/components/Gradient/index.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+export default function Gradient() {
+  return (
+    <svg
+      width="1512"
+      height="1426"
+      viewBox="0 0 1512 1426"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="gradient"
+    >
+      <g filter="url(#filter0_f_311_31)">
+        <ellipse
+          rx="131.079"
+          ry="144.161"
+          transform="matrix(1 0 0 -1 131.079 144.161)"
+          fill="#C850C0"
+        />
+      </g>
+      <g filter="url(#filter1_f_311_31)">
+        <ellipse
+          rx="220.077"
+          ry="242.042"
+          transform="matrix(1 0 0 -1 821.781 739.958)"
+          fill="#FFCC70"
+        />
+      </g>
+      <g filter="url(#filter2_f_311_31)">
+        <ellipse
+          rx="142.203"
+          ry="156.397"
+          transform="matrix(1 0 0 -1 1369.8 196.826)"
+          fill="#4158D0"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_311_31"
+          x="-444"
+          y="-444"
+          width="1150.16"
+          height="1176.32"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="222"
+            result="effect1_foregroundBlur_311_31"
+          />
+        </filter>
+        <filter
+          id="filter1_f_311_31"
+          x="157.704"
+          y="53.9155"
+          width="1328.15"
+          height="1372.08"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="222"
+            result="effect1_foregroundBlur_311_31"
+          />
+        </filter>
+        <filter
+          id="filter2_f_311_31"
+          x="783.593"
+          y="-403.571"
+          width="1172.41"
+          height="1200.79"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="222"
+            result="effect1_foregroundBlur_311_31"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
