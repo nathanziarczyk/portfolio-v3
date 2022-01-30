d4695074ea3ee0736461d8624dc7c6c68669df19
Nathan Ziarczyk <nathan.ziar@gmail.com>
2022-01-30 21:19:44 +0100

    feat: added honeypot field to contact form

--- src/components/slices/ContactForm/index.jsx
+++ src/components/slices/ContactForm/index.jsx
@@ -4,23 +4,29 @@ import { Button } from "../../Button/style";
 
 const ContactForm = () => {
   return (
-    <Form>
+    <Form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+    >
+      <input name="bot-field" className="hidden" />
       <FormRow>
         <label htmlFor="name">
           Name
-          <input type="text" id="name" placeholder="Name" />
+          <input type="text" id="name" placeholder="Name" required />
         </label>
       </FormRow>
       <FormRow>
         <label htmlFor="email">
           Email address
-          <input type="email" id="email" placeholder="Email address" />
+          <input type="email" id="email" placeholder="Email address" required />
         </label>
       </FormRow>
       <FormRow>
         <label htmlFor="message">
           Message
-          <textarea id="message"></textarea>
+          <textarea id="message" required></textarea>
         </label>
       </FormRow>
       <Button>Submit</Button>
