c6fe754ac546fa5762d52d97cc15afa19548fb50
Nathan Ziarczyk <nathan.ziar@gmail.com>
2022-02-21 14:21:02 +0100

    generate links with locale prefixes

--- prismic/linkResolver.js
+++ prismic/linkResolver.js
@@ -1,7 +1,12 @@
 const linkResolver = (link) => {
-  if (link.link_type === "Document")
-    return link.uid === "home" ? "/" : "/" + link.uid;
+  if (["Document", "page"].includes(link.link_type)) {
+    const prefix = link.lang === "nl-be" ? "" : link.lang.slice(0, 2);
+    return link.uid === "home"
+      ? `/${prefix}`
+      : `${prefix ? `/${prefix}` : ""}/${link.uid}`;
+  }
   if (link.link_type === "Web") return link.url;
+
   // Fallback for other types, in case new custom types get created
   return "/doc/" + link.id;
 };
