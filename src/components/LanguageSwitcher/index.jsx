ed8dfc75ceadc606e43e09cc3b4141b129918448
Nathan Ziarczyk <nathan.ziar@gmail.com>
2022-02-22 14:38:12 +0100

    added arrow

--- src/components/LanguageSwitcher/index.jsx
+++ src/components/LanguageSwitcher/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { LanguageSelector } from "./style";
+import { LanguageSelector, SelectorWrapper } from "./style";
 import { navigate } from "gatsby";
 import linkResolver from "../../../prismic/linkResolver";
 
@@ -8,17 +8,22 @@ export default function LanguageSwitcher({ langData }) {
     navigate(event.target.value);
   };
   return (
-    <LanguageSelector defaultValue={langData.lang} onChange={handleLangChange}>
-      <option value={langData.lang}>
-        {langData.lang.slice(0, 2).toUpperCase()}
-      </option>
-      {langData.altLangs.map((altLang) => {
-        return (
-          <option value={linkResolver(altLang)} key={altLang.id}>
-            {altLang.lang.slice(0, 2).toUpperCase()}
-          </option>
-        );
-      })}
-    </LanguageSelector>
+    <SelectorWrapper>
+      <LanguageSelector
+        defaultValue={langData.lang}
+        onChange={handleLangChange}
+      >
+        <option value={langData.lang}>
+          {langData.lang.slice(0, 2).toUpperCase()}
+        </option>
+        {langData.altLangs.map((altLang) => {
+          return (
+            <option value={linkResolver(altLang)} key={altLang.id}>
+              {altLang.lang.slice(0, 2).toUpperCase()}
+            </option>
+          );
+        })}
+      </LanguageSelector>
+    </SelectorWrapper>
   );
 }
--- src/components/LanguageSwitcher/style.js
+++ src/components/LanguageSwitcher/style.js
@@ -1,12 +1,32 @@
 import styled from "styled-components";
+
+export const SelectorWrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+  &::after {
+    content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='17' height='17' viewBox='0 0 24 24' fill='none' stroke='%23000000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M6 9l6 6 6-6'/%3E%3C/svg%3E");
+    position: absolute;
+    right: 10px;
+    z-index: 3;
+    top: 50%;
+    transform: translate(40%, -50%);
+  }
+`;
+
 export const LanguageSelector = styled.select`
+  cursor: pointer;
+  font-size: 1rem;
   position: relative;
   z-index: 2;
-  padding: 0 0.5rem;
+  padding: 0.5rem;
+  height: max-content;
+  vertical-align: bottom;
+  margin: auto;
   border: 1px solid ${({ theme }) => theme.text};
   background-color: ${({ theme }) => theme.bg};
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
   border-radius: 0;
+  padding-right: 20px;
 `;
